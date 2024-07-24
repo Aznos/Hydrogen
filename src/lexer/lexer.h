a69f8798be246633dc2102b652bf478de7091435
Aznos <aznosreal@gmail.com>
2024-07-24 15:14:50 -0400

    enum

--- src/lexer/lexer.h
+++ src/lexer/lexer.h
@@ -1,4 +1,14 @@
 #ifndef LEXER_H
 #define LEXER_H
 
-#endif LEXER_H
+typedef enum {
+    TOKEN_VOID,
+    TOKEN_IDENTIFIER,
+    TOKEN_LEFT_PAREN,
+    TOKEN_RIGHT_PAREN,
+    TOKEN_LEFT_BRACE,
+    TOKEN_RIGHT_BRACE,
+    TOKEN_EOF
+};
+
+#endif
