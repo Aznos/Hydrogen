97942ff34b783eb20d306d5953545f8d89fd4bd7
Aznos <aznosreal@gmail.com>
2024-07-24 15:07:47 -0400

    Read from file

--- src/main.c
+++ src/main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello world!\n");
+int main(int argc, char* argv[]) {
+    FILE* file = fopen("./src/hydrogen/test.hydro", "r");
+    char fileContents[1000];
+
+    if(file != NULL) {
+        while(fgets(fileContents, sizeof(fileContents), file) != NULL) {
+            printf("%s", fileContents);
+        }
+    } else {
+        printf("File not found!\n");
+    }
+
+    fclose(file);
 }
