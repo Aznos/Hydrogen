8ac0de5b3ffcad4f0a6608924dfbca075c19e9f0
Aznos <aznosreal@gmail.com>
2024-07-24 21:28:19 -0400

    Used linkedlist in armcodegen

--- src/codegen/arm_codegen.c
+++ src/codegen/arm_codegen.c
@@ -13,8 +13,16 @@ void generateARMAssembly(FunctionNode* function, const char* outputFileName) {
     fprintf(outputFile, "    stp x29, x30, [sp, -16]!\n"); // Save the frame pointer and link register
     fprintf(outputFile, "    mov x29, sp\n"); // Set the frame pointer to the current stack pointer
 
-    fprintf(outputFile, "    mov w0, 0\n"); // Set the return value to 0 (since no body is implemented)
+    VariableNode* var = function->variables;
+    while(var != NULL) {
+        fprintf(outputFile, "    sub sp, sp, #4\n"); //Allocate space for each variable
+        fprintf(outputFile, "    mov w0, #%d\n", var->value); // Set the value of the variable
+        fprintf(outputFile, "    str w0, [sp, #-4]\n"); // Store the value of the variable in the allocated space
+    
+        var = var->next;
+    }
 
+    fprintf(outputFile, "    mov w0, 0\n"); // Set the return value to 0 (since no body is implemented)
     fprintf(outputFile, "    ldp x29, x30, [sp], 16\n"); // Restore the frame pointer and link register
     fprintf(outputFile, "    ret\n"); // Return
 
