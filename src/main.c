586a763df55f93f07b6f2c7aa4b20ed20c4b7874
Aznos <aznosreal@gmail.com>
2024-07-24 21:31:31 -0400

    keepasm arg

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include "lexer/lexer.h"
 #include "parser/parser.h"
@@ -7,14 +8,24 @@
 #include "codegen/arm_codegen.h"
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <filename>\n", argv[0]);
+    if (argc < 2 || argc > 3) {
+        printf("Usage: %s <filename> [--keepasm]\n", argv[0]);
         return 1;
     }
 
     char assemblyFile[] = "temp.s";
     char objectFile[] = "temp.o";
     char executableFile[] = "main";
+    int keepAsm = 0;
+
+    if (argc == 3) {
+        if (strcmp(argv[2], "--keepasm") == 0) {
+            keepAsm = 1;
+        } else {
+            printf("Unknown option: %s\n", argv[2]);
+            return 1;
+        }
+    }
 
     FILE* file = fopen(argv[1], "r");
     if (file == NULL) {
@@ -29,14 +40,26 @@ int main(int argc, char* argv[]) {
 
         char assembleCmd[512];
         snprintf(assembleCmd, sizeof(assembleCmd), "as -o %s %s", objectFile, assemblyFile);
-        system(assembleCmd);
+        int assembleStatus = system(assembleCmd);
+        if (assembleStatus != 0) {
+            printf("Assembly failed with status: %d\n", WEXITSTATUS(assembleStatus));
+            fclose(file);
+            return 1;
+        }
 
         char linkCmd[512];
         snprintf(linkCmd, sizeof(linkCmd), "gcc -o %s %s", executableFile, objectFile);
-        system(linkCmd);
+        int linkStatus = system(linkCmd);
+        if (linkStatus != 0) {
+            printf("Linking failed with status: %d\n", WEXITSTATUS(linkStatus));
+            fclose(file);
+            return 1;
+        }
 
-        remove(assemblyFile);
-        remove(objectFile);
+        if (!keepAsm) {
+            remove(assemblyFile);
+            remove(objectFile);
+        }
     } else {
         printf("Parsing failed\n");
     }
