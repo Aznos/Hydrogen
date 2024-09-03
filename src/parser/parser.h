da94b68127ca9d700e74e969dad120ae339b63d3
Aznos <aznosreal@gmail.com>
2024-09-03 12:49:50 -0400

    Working on parser.h

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -1,6 +1,16 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
 
+extern int currentTok;
+extern int getNextToken() {return currentTok = gettok();}
 
 #endif
