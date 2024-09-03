f3f8d00ff7c39564f041253f744d714a785b3ba7
Aznos <aznosreal@gmail.com>
2024-09-03 12:28:53 -0400

    AST namespaces

---
+++ src/parser/ast.h
@@ -0,0 +1,69 @@
+#ifndef AST_H
+#define AST_H
+
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace {
+    class ExprAST { //Base class for all expression nodes
+        public:
+            virtual ~ExprAST() = default;
+    };
+
+    class NumberExprAST : public ExprAST { //Expression class for numeric literals
+        double val;
+
+        public:
+            NumberExprAST(double val) : Val(Val) {}
+    };
+
+    class VariableExprAST : public ExprAst { //Expression class for referencing a variable
+        std::string name;
+
+        public:
+            VariableExprAST(const std::string &name) : name(name) {}
+    };
+
+    class BinaryExprAST : public ExprAST { //Expression class for a binary operation
+        char op; //Op code
+        std::unique_ptr<ExprAST> LHS, RHS; //Left and right side of the equasion
+
+        public:
+            BinaryExprAST(char op, std::unique_ptr<ExprAST> LHS, std::unique_ptr<ExprAST> RHS) : op(op), LHS(std::move(LHS)), RHS(std:move(RHS)) {}
+    };
+
+    class CallExprAST : public ExprAST { //Expression class for function calls
+        std::string callee; //What function the function was called from
+        std::vector<std::unique_ptr<ExprAST>> args;
+
+        public:
+            CallExprAST(const std::string &callee, std::vector<std::unique_ptr<ExprAST>> args) : callee(callee), args(std::move(args)) {}
+    };
+
+    class PrototypeAST { //Represents a prototype for a function which takes a name and argument names
+        std::string name;
+        std::vector<std::string> args;
+
+        public:
+            PrototypeAST(const std::string &name, std::vector<std::string> args) : name(name), args(std::move(args)) {}
+            const std::string &getName() const {
+                return name;
+            }
+    };
+
+    class FunctionAST { //Represents a function definition
+        std::unique_ptr<PrototypeAST> prototype;
+        std::unique_ptr<ExprAST> body;
+
+        public:
+            FunctionAST(std::unique_ptr<PrototypeAST> prototype, std::unique_ptr<ExprAST> body) : prototype(std:move(prototype)), body(std::move(body)) {}
+    };
+}
+
+#endif
